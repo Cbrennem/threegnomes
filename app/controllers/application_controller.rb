ba7448833bf279370257d843a9efffccc09dfda9
C <Cbrennem82@gmail.com>
2018-10-19 21:07:36 +0900

    Yui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello 3 Gnomes!"
+    render html: "Hello 3 Gnomes!  Hi Yui, You are lovely and beautiful!  I love you!"
   end
 
 end
