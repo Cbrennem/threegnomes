68ad49c62348bbc76fc159cb3c83b98640924d22
Chris Brenneman <cbrenneman@inalabjapan.co.jp>
2018-10-23 17:17:33 +0900

    Added Static pages and removed previous tutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Hello 3 Gnomes!  Hi Yui, You are lovely and beautiful!  I love you!"
-  end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,5 @@ ActiveRecord::Schema.define(version: 20181022081455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
